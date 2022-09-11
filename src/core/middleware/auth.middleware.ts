baf880a5b69f2d1fa62442d3dbef91b2c327dae2
nguyenquan0210 <nguyenquanute0210@gmail.com>
2022-09-11 10:37:23 +0700

    Add APIs to user (update and get by id)

--- src/core/middleware/auth.middleware.ts
+++ src/core/middleware/auth.middleware.ts
@@ -6,8 +6,6 @@ import jwt from 'jsonwebtoken';
 const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
     const token = req.header('x-auth-token');
 
-    console.log(token);
-
     if (!token)
         return res.status(401).json({ message: 'No token, authorization denied.' });
 
